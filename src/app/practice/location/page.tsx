9401a9aa8d1c6069a210ac2a2dbd474bce530e4c
최가오 <44205245+sionel@users.noreply.github.com>
2023-08-06 16:39:53 +0900

    Feat : create empty page and Link

---
+++ src/app/practice/location/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Typing from "../components/Typing";
+
+const Location = () => {
+  return (
+    <Typing>
+      <div>Location</div>
+    </Typing>
+  );
+};
+
+export default Location;
